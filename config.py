c7aa1cc2d764d4986a340ad92de66230f2b9f053
Ameer Adeigbe <ameeradeigbe@gmail.com>
2024-09-03 19:19:37 +0100

    Initial commit

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'  # Replace with your SMTP server
     MAIL_PORT = 587  # Typically 587 for TLS, 465 for SSL
-    MAIL_USERNAME = 'ameeradeigbe@gmail.com'  # Replace with your email address
-    MAIL_PASSWORD = 'dbgtwfeyvsrasotn '  # Replace with your email password
+    MAIL_USERNAME = '#'  # Replace with your email address
+    MAIL_PASSWORD = '#'  # Replace with your email password
     MAIL_USE_TLS = True  # Use TLS
     MAIL_USE_SSL = False  # Do not use SSL
-    MAIL_DEFAULT_SENDER = 'ameeradeigbe@gmail.com'  # Default sender
+    MAIL_DEFAULT_SENDER = '#'  # Default sender
